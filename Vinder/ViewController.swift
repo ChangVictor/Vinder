115ba1944312a2bbc438180dc29c45b10aca915b
Victor Chang <v.chang.yu@gmail.com>
2018-12-25 12:54:23 -0300

    first commit

--- Vinder/ViewController.swift
+++ Vinder/ViewController.swift
@@ -12,7 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let subviews =  [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+            
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+         stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        stackView.fillSuperview()
     }
 
 
