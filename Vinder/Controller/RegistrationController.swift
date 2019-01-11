8a14a8a26f4c0ba75687884b99ffd930c3632afa
Victor Chang <v.chang.yu@gmail.com>
2019-01-11 11:23:54 -0300

    register textfields

--- Vinder/Controller/RegistrationController.swift
+++ Vinder/Controller/RegistrationController.swift
@@ -17,46 +17,40 @@ class RegistrationController: UIViewController {
         button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
         button.backgroundColor = .white
         button.setTitleColor(.black, for: .normal)
-        button.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
         button.layer.cornerRadius = 16
         return button
     }()
     
-    let fullNameTextField: ConstantTextField = {
-        let tf = ConstantTextField()
+    let fullNameTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 16)
         tf.placeholder = "Enter full name"
         tf.backgroundColor = .white
+        tf.layer.cornerRadius = 25
         
         return tf
     }()
     
-    let emailTextField: ConstantTextField = {
-        let tf = ConstantTextField()
+    let emailTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 16)
         tf.placeholder = "Enter email"
         tf.keyboardType = .emailAddress
         tf.backgroundColor = .white
         return tf
     }()
     
-    let passwordTextField: ConstantTextField = {
-        let tf = ConstantTextField()
+    let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 16)
         tf.placeholder = "Enter password"
         tf.backgroundColor = .white
         tf.isSecureTextEntry = true
         return tf
     }()
-    
-    // internal class
-    class ConstantTextField: UITextField {
         
-        override var intrinsicContentSize: CGSize {
-            return .init(width: 0, height: 50)
-        }
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupGradientLayer()
         
         view.backgroundColor = .red
         
@@ -71,9 +65,21 @@ class RegistrationController: UIViewController {
         stackView.axis = .vertical
         stackView.spacing = 8
         
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
         
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setupGradientLayer() {
+        
+        let gradientLayer = CAGradientLayer()
+        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.bounds
+        
     }
 
 
---
+++ Vinder/Views/CustomTextField.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTextField.swift
+//  Vinder
+//
+//  Created by Victor Chang on 11/01/2019.
+//  Copyright © 2019 Victor Chang. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    
+    init(padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        layer.cornerRadius = 25
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0 )
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
