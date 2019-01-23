fe5bd3ff55977935e1eea339eee783f10c707c37
Victor Chang <v.chang.yu@gmail.com>
2019-01-23 19:32:20 -0300

    setting form rows

--- Vinder/Controller/SettingsController.swift
+++ Vinder/Controller/SettingsController.swift
@@ -50,10 +50,11 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationItems()
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         tableView.tableFooterView = UIView()
+        tableView.keyboardDismissMode = .interactive
 
-        setupNavigationItems()
     }
     
     fileprivate func setupNavigationItems() {
@@ -67,9 +68,8 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         ]
     }
     
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    lazy var header: UIView = {
         let header = UIView()
-        
         header.addSubview(imageOneButton)
         let padding: CGFloat = 16
         imageOneButton.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
@@ -84,12 +84,64 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         stackView.anchor(top: header.topAnchor, leading: imageOneButton.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
         
         return header
+    }()
+    
+    class HeaderLabel: UILabel {
+        override func drawText(in rect: CGRect) {
+            super.drawText(in: rect.insetBy(dx: 16, dy: 0))
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {        
+        if section == 0 {
+            return header
+        }
+        
+        let headerLabel = HeaderLabel()
+        switch section {
+        case 1:
+            headerLabel.text = "Name"
+        case 2:
+            headerLabel.text = "Profession"
+        case 3:
+            headerLabel.text = "Age"
+        default:
+            headerLabel.text = "Bio"
+        }
+        return headerLabel
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 300
+        if section == 0 {
+            return 300
+        }
+        return 40
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 0 : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SettingsCell(style: .default, reuseIdentifier: nil)
+        
+        switch indexPath.section {
+        case 1:
+            cell.textField.placeholder = "Enter Name"
+        case 2:
+            cell.textField.placeholder = "Enter Profession"
+        case 3:
+            cell.textField.placeholder = "Enter Age"
+        default:
+            cell.textField.placeholder = "Enter Bio"
+        }
+        
+        return cell
+    }
     
     @objc fileprivate func handleCancel() {
         dismiss(animated: true, completion: nil)
